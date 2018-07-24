5f1fd98e67ac6aa377fffc4704b9ccac966504fa
Victor Gutemberg Oliveira Marques <violivei@microsoft.com>
2018-07-24 15:20:53 -0700

    K and Threshold as global variables

--- chord_recognition.py
+++ chord_recognition.py
@@ -7,17 +7,18 @@ file_path = sys.argv[1]
 y, sr = librosa.load(file_path)
 
 chroma = librosa.feature.chroma_cqt(y, sr)
-print(chroma)
+K = 1/2
+THRESHOLD = 0.35
 
 for i in range(len(chroma)):
     for j in range(len(chroma[i])):
         neighbor1 = (i + 1)%len(chroma)
 
-        chroma[i][j] = chroma[i][j] - (chroma[i - 1][j] + chroma[neighbor1][j])*(1/2)
+        chroma[i][j] = chroma[i][j] - (chroma[i - 1][j] + chroma[neighbor1][j])*K
 
         chroma[i][j] = chroma[i][j] if chroma[i][j] > 0 else 0
 
-chroma[chroma < 0.35] = 0
+chroma[chroma < THRESHOLD] = 0
 librosa.display.specshow(chroma, x_axis='time', y_axis='chroma')
 plt.colorbar()
 plt.show()
