4e33fc3eedd398dba9c912698489b322ac8b54ef
Victor Gutemberg Oliveira Marques <violivei@microsoft.com>
2018-07-24 14:57:53 -0700

    Adding neighbor correction

--- chord_recognition.py
+++ chord_recognition.py
@@ -8,7 +8,16 @@ y, sr = librosa.load(file_path)
 
 chroma = librosa.feature.chroma_cqt(y, sr)
 print(chroma)
-# chroma[chroma < 0.5] = 0
+
+for i in range(len(chroma)):
+    for j in range(len(chroma[i])):
+        neighbor1 = (i + 1)%len(chroma)
+
+        chroma[i][j] = chroma[i][j] - (chroma[i - 1][j] + chroma[neighbor1][j])*(1/2)
+
+        chroma[i][j] = chroma[i][j] if chroma[i][j] > 0 else 0
+
+chroma[chroma < 0.35] = 0
 librosa.display.specshow(chroma, x_axis='time', y_axis='chroma')
 plt.colorbar()
 plt.show()
